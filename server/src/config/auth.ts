18d2eab4ce079886b5738e68a588479f427adb0b
Samuel Dantas Cavalcante Evangelista <samuel-icm-dantas@hotmail.com>
2021-01-14 22:00:56 -0300

    Only FrontEnd now

--- server/src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  jwt: {
-    secret: 'cf8394f0c1c50e21c2f26903705bb094',
-    expiresIn: '1d',
-  },
-};
--- server/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++
@@ -1,7 +0,0 @@
-import Appointment from '../infra/typeorm/entities/Appointment';
-import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
-
-export default interface IAppointmentsRepository {
-  create(data: ICreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
-}
